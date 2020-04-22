f97f12c6b4c7f32e334cf338fed7b2eb568ac302
Tyler Eddy <tylereddy96@gmail.com>
2020-04-22 17:02:13 +0100

    simplified sligtly

--- src/main/java/com/tyler/account/service/AccountPrizeService.java
+++ src/main/java/com/tyler/account/service/AccountPrizeService.java
@@ -14,29 +14,27 @@ public class AccountPrizeService {
 
 	public Account decidePrize(Account account) {
 		int accountNumberLength = account.getAccountNumber().length();
+		if (account.getAccountNumber().startsWith("a")) {
+			account.setPrize(new Prize(null, 0));
+		}
 
-		if (accountNumberLength == 6) {
-			if (account.getAccountNumber().startsWith("a")) {
-				account.setPrize(new Prize(null, 0));
-			} else if (account.getAccountNumber().startsWith("b")) {
+		else if (accountNumberLength == 6) {
+
+			if (account.getAccountNumber().startsWith("b")) {
 				account.setPrize(new Prize(null, 50));
 			} else if (account.getAccountNumber().startsWith("c")) {
 				account.setPrize(new Prize(null, 100));
 			}
 
 		} else if (accountNumberLength == 8) {
-			if (account.getAccountNumber().startsWith("a")) {
-				account.setPrize(new Prize(null, 0));
-			} else if (account.getAccountNumber().startsWith("b")) {
+			if (account.getAccountNumber().startsWith("b")) {
 				account.setPrize(new Prize(null, 500));
 			} else if (account.getAccountNumber().startsWith("c")) {
 				account.setPrize(new Prize(null, 750));
 			}
 
 		} else if ((accountNumberLength == 10)) {
-			if (account.getAccountNumber().startsWith("a")) {
-				account.setPrize(new Prize(null, 0));
-			} else if (account.getAccountNumber().startsWith("b")) {
+			if (account.getAccountNumber().startsWith("b")) {
 				account.setPrize(new Prize(null, 5000));
 			} else if (account.getAccountNumber().startsWith("c")) {
 				account.setPrize(new Prize(null, 10000));
